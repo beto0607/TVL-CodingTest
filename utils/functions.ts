d0ae45185c9678b84989b32f79dfc081b40b6031
Roberto Albanesi <beto0607@gmail.comn>
2019-08-19 17:48:08 +0200

    Added function to check if an Y value is in a box(y and heigth)

--- utils/functions.ts
+++ utils/functions.ts
@@ -1,3 +1,5 @@
 import { Topic } from "../types/types";
 
-export const compareTopics = (a: Topic, b: Topic) => a.id === b.id
+export const compareTopics = (a: Topic, b: Topic): boolean => a.id === b.id
+
+export const checkIfYInBox = (y: number, box: { y: number, heigth: number }): boolean => y >= box.y && y <= (box.y + box.heigth)
